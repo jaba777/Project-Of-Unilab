379002e9db804f51fbdd291147cd3d5cc960da7b
jaba777 <tskhovreba777@gmail.com>
2022-06-12 20:24:50 +0400

    add remove

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,11 @@
 const burgerMenu=document.querySelector('.menu-Burger');
 const burgerTitle=document.querySelector('.burger-menu');
+const deleteMenu=document.querySelector('.delete-menu');
 
 burgerMenu.addEventListener('click',()=>{
     burgerTitle.classList.add('active');
+})
+
+deleteMenu.addEventListener('click',()=>{
+    burgerTitle.classList.remove('active');
 })
